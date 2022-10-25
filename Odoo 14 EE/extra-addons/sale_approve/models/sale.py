e38b31c603cfa6db2b8b97257f2b2701f2045b4a
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2022-10-24 23:38:25 -0500

    Correct approve flow.

--- Odoo 14 EE/extra-addons/sale_approve/models/sale.py
+++ Odoo 14 EE/extra-addons/sale_approve/models/sale.py
@@ -7,6 +7,7 @@ class Sale(models.Model):
     approve_state = fields.Selection(string='Estado de Aprobación', selection=APPROVE_STATES,track_visibility='onchange')
     SHIPMENT_STATES = [('draft', 'Solicitud de Embarques No Aprobada'),('done', 'Solicitud de Embarques Aprobada')]
     shipment_state = fields.Selection(string='Solicitud de Embarque', selection=SHIPMENT_STATES,track_visibility='onchange',default='draft')
+    payment_term_days = fields.Integer(compute='_compute_payment_term_days',string='Días de Crédito')
 
     def sale_approve_state_draft(self):
         for record in self:
@@ -27,18 +28,27 @@ class Sale(models.Model):
     def sale_shipment_state_done(self):
         for record in self:
             record.shipment_state = 'done'   
-
-    #  = fields.Integer(related='payment_term_id.days', string='Días de Crédito')
-    
-    payment_term_days = fields.Integer(compute='_compute_payment_term_days',string='Días de Crédito')
-    
     
     def _compute_payment_term_days(self):
         for record in self:
-            print(record.payment_term_id.line_ids.days)
-            self.payment_term_days = record.payment_term_id.line_ids.days
-        
+            record.payment_term_days = record.payment_term_id.line_ids.days
+
+    ready_to_shipment_validation = fields.Boolean(compute="_compute_ready_to_shipment_validation", help="Whether or not this line should display a button allowing to remove its related payments from the batch")
+
+    def _compute_ready_to_shipment_validation(self):
+        for record in self:
+            result = False
+            if record.payment_term_days>0 and record.approve_state=='done' and record.shipment_state not in ['done']:
+                result = True
+            elif record.payment_term_days==0 and record.approve_state=='confirm' and record.shipment_state not in ['done']:
+                result = True                     
+            record.ready_to_shipment_validation = result
+
+
+
+
     
+
 
     
     
