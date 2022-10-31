b9b0481ea4d0f157b1337a221bf9376755edd591
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2022-10-31 13:01:11 -0600

    Migration from studio to sale_approve module updated.

--- Odoo 14 EE/extra-addons/sale_approve/migration/migration.py
+++ Odoo 14 EE/extra-addons/sale_approve/migration/migration.py
@@ -25,5 +25,51 @@ for record in all_sale_objects:
     count = count +1
     print('%s de %s'%(count,total))
 
+
+count = 0
+stock_picking = self.env['stock.picking'].search([])
+total = len(stock_picking)
+for record in stock_picking:
+    if record.state in ['done']:
+        if record.sale_id:
+            if record.sale_id.shipment_state in ['done']:
+                record.shipment_state = 'done'
+        
+        count = count +1
+        print('%s de %s'%(count,total))
+
+count = 0
+sales_order = self.env['sale.order'].search([])
+total = len(sales_order)
+err = []
+for record in sales_order:
+    try:
+        state = dict(record._fields['state'].selection).get(record.state)
+        print(state)
+        if state in ['Sales Order']:
+            for rec in record.picking_ids:
+                if rec.state  in ['done']:
+                    record.shipment_state = 'done'
+        if state in ['Locked']:
+            record.action_unlock()
+            print(record.state)
+            if state in ['Sales Order']:
+                for rec in record.picking_ids:
+                    if rec.state  in ['done']:
+                        record.shipment_state = 'done'   
+            record.action_done()
+            print(state)                    
+        count = count +1
+        print('%s de %s'%(count,total))
+    except:
+        count = count +1
+        print('%s de %s'%(count,total))
+        err.append(record.id)
+
+                
+
+
+
+
 ##  python3 -m ptvsd --host localhost --port 5678 /usr/bin/odoo --config /etc/odoo/odoo.conf --xmlrpc-port=8001 --workers=0 -u sale_approve
-##  /usr/bin/odoo shell -d SALE_APPROVE_TEST --config /etc/odoo/odoo.conf --xmlrpc-port=8001
+##  /usr/bin/odoo shell -d SALE_APPROVE --config /etc/odoo/odoo.conf --xmlrpc-port=8001 --workers=8
