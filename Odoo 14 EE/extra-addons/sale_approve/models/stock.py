da2faba41d7aaf357a506b0ca33b3fbbfadebb1b
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2022-11-01 15:49:41 -0600

    shipment_state as computed field.

--- Odoo 14 EE/extra-addons/sale_approve/models/stock.py
+++ Odoo 14 EE/extra-addons/sale_approve/models/stock.py
@@ -4,8 +4,19 @@ from odoo.exceptions import ValidationError
 class Stock(models.Model):
     _inherit = 'stock.picking'
     SHIPMENT_STATES = [('draft', 'Solicitud de Embarque No Aprobada'),('done', 'Solicitud de Embarque Aprobada')]
-    shipment_state = fields.Selection(selection=SHIPMENT_STATES,default='draft',string='Solicitud de Embarque')
+    # shipment_state = fields.Selection(selection=SHIPMENT_STATES,default='draft',string='Solicitud de Embarque')
     sale_is_set = fields.Boolean(compute='_compute_sale_is_set', string='Tiene Cotización ó Pedido de Venta Asociado')
+    shipment_state = fields.Selection(compute='_compute_shipment_state',selection=SHIPMENT_STATES,default='draft',string='Solicitud de Embarque')
+    
+
+    
+    def _compute_shipment_state(self):
+        for record in self:
+            if record.sale_id.shipment_state in ['done']:
+                record.shipment_state = 'done'
+            else:
+                record.shipment_state = 'draft'
+
     
 
     def _compute_sale_is_set(self):
